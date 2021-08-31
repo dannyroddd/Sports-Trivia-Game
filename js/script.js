2742c1b6ff1f6813bde72ade7db2335a7d4a9ffc
Daniel Rodriguez <danielrodriguez@Daniels-MBP.home>
2021-08-31 17:37:42 -0400

    updated game board and updated js with jquery dom variables

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
+//Contentful url and API key containing objects with the 30 trivia questions I came up with
+
 const url = 'https://cdn.contentful.com/spaces/0pgg3owegtt8/environments/master/entries?access_token=nU633R4z0XlML3L6u_6hLTxBQq11TVtEi_i2K_98EAo&content_type=sportsTriviaQuestions'
 $.ajax(url)
 .then((data) =>{
     console.log(data)
 })
+
+//game state
+const game = {
+    p1: 0,
+    p2: 0,
+    question:{} 
+    }
+
+//dom variables for game pieces
+const $question = $('#question')
+const $answer = $('#answer')
+const $a = $('#a')
+const $b = $('#b')
+const $c = $('#c')
+const $c = $('#c')
+const $d = $('#d')
+const $scores = $('#scores')
+const $p1 = $('#p1')
+const $p1score = $('#p1score')
+const $p2 = $('#p2')
+const $p2score = $('#p2score')
+const $reset = $('#reset')
+const $button = $('#button')
+    
